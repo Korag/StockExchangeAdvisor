0dd23e458948eab1dca2cc4b168ce78e42015ad8
Łukasz Czepielik <koragrex@gmail.com>
2020-04-09 02:04:56 +0200

    Added RabbitMQ consumers .NET Core projects.

---
+++ RabbitMQConsumerEMA/Program.cs
@@ -0,0 +1,19 @@
+﻿using TechnicalIndicators;
+using RabbitMQ;
+
+namespace RabbitMQConsumerEMA
+{
+    public class RabbitConsumerCalculateEMA
+    {
+        private const string _queueReceiveFrom = "ema_tobecalculated";
+        private const string _queueSendTo = "ema_signals";
+        private const int _interval = 1000;
+ 
+        static void Main()
+        {
+            TechnicalIndicator _indicator = new TechnicalIndicatorEMA();
+            RabbitCalculateIndicator rabbitEMA = new RabbitCalculateIndicator(_queueReceiveFrom, _queueSendTo, _interval, _indicator);
+            rabbitEMA.ConsumeData();
+        }
+    }
+}
---
+++ RabbitMQConsumerROC/Program.cs
@@ -0,0 +1,19 @@
+﻿using TechnicalIndicators;
+using RabbitMQ;
+
+namespace RabbitMQConsumerROC
+{
+    class Program
+    {
+        private const string _queueReceiveFrom = "roc_tobecalculated";
+        private const string _queueSendTo = "roc_signals";
+        private const int _interval = 1000;
+
+        static void Main()
+        {
+            TechnicalIndicator _indicator = new TechnicalIndicatorROC();
+            RabbitCalculateIndicator rabbitROC = new RabbitCalculateIndicator(_queueReceiveFrom, _queueSendTo, _interval, _indicator);
+            rabbitROC.ConsumeData();
+        }
+    }
+}
