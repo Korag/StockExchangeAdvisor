936ef9768c66812ada4c989c3c53c929f341f141
Łukasz Czepielik <koragrex@gmail.com>
2020-04-09 02:02:47 +0200

    Added Utility project.

---
+++ Utility/CsvHelper.cs
@@ -0,0 +1,42 @@
+﻿using CsvHelper;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace Utility
+{
+    public static class CsvHelper
+    {
+        public static List<Quote> ReadSingleCsvFileWithQuotes(string path)
+        {
+            try
+            {
+                IEnumerable<Quote> readQuotes = new List<Quote>();
+
+                using (var reader = new StreamReader(path))
+                {
+                    // parsing of date
+
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        csv.Configuration.HasHeaderRecord = true;
+                        csv.Configuration.RegisterClassMap<ReadFromCSVViewModelMapper>();
+                        csv.Configuration.Delimiter = ",";
+
+                        readQuotes = csv.GetRecords<Quote>();
+
+                        return readQuotes.ToList();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+
+}
---
+++ Utility/ZipHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace Utility
+{
+    public static class ZipHelper
+    {
+        public static void ExtractZipDirectory(string zipPath, string extractPath)
+        {
+            try
+            {
+                // check directory if is empty
+                // if yes extract
+                // if no delete all files
+
+                using (ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Update))
+                {
+                    archive.ExtractToDirectory(extractPath);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
