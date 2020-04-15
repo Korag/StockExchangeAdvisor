3b39bd97342b22316ed02cbf0bc6ad3cab3f78c8
Łukasz Czepielik <koragrex@gmail.com>
2020-04-15 20:14:22 +0200

    Added one-way RabbitMQ producer class.

---
+++ RabbitMQ/Abstract/IRabbitMQSendQuotesToCalculation.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using RabbitMQ.Client;
+
+namespace RabbitMQ
+{
+    public abstract class IRabbitMQSendQuotesToCalculation
+    {
+        public string _exchange;
+        public string _queueSendTo;
+
+        public IRabbitMQSendQuotesToCalculation(string exchange, string queueSendTo)
+        {
+            _exchange = exchange;
+            _queueSendTo = queueSendTo;
+        }
+
+        public abstract void Process(IndicatorCalculationElements elements);
+        public abstract void GenerateAndPublishMessage(IModel channel, IndicatorCalculationElements elements);
+    }
+}
--- RabbitMQ/RabbitMQSendQuotesToCalculation.cs
+++ RabbitMQ/RabbitMQSendQuotesToCalculation.cs
@@ -1,50 +1,54 @@
-﻿//using Models;
-//using RabbitMQ.Client;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using Utility;
+﻿using Models;
+using RabbitMQ.Client;
+using System;
+using Utility;
 
-//namespace RabbitMQ
-//{
-//    class RabbitMQSendQuotesToCalculation
-//    {
-//        private void Process(IndicatorCalculationElements elements)
-//        {
-//            try
-//            {
-//                using (var connection = RabbitMQConnectionHelper.GetConnection())
-//                {
-//                    using (var channel = connection.CreateModel())
-//                    {
-//                        //umożliwia potwierdzanie, że przesyłkę dostarczono do Exchange
-//                        channel.ConfirmSelect();
-//                        while (true)
-//                        {
-//                            //generujemy nowe konto
-//                            GenerateAndPublishMessage(channel, elements);
-//                        }
-//                    }
-//                }
-//            }
-//            catch (Exception e)
-//            {
-//                Console.WriteLine(e);
-//                throw;
-//            }
-//        }
+namespace RabbitMQ
+{
+    public class RabbitMQSendQuotesToCalculation : IRabbitMQSendQuotesToCalculation
+    {
+        public RabbitMQSendQuotesToCalculation(string exchange, string queueSendTo) : base(exchange, queueSendTo)
+        {
+        }
 
-//        private static void GenerateAndPublishMessage(IModel channel, IndicatorCalculationElements elements)
-//        {
-//            //serializujemy oraz przekształcamy w tablicę bajtów
-//            var body = Utils.StringToUtf8(Utils.AccountToJsonString(newAccount));
-//            channel.BasicPublish(exchange: _exchange,
-//                routingKey: _routingKey,
-//                basicProperties: null,
-//                body: body);
-//            //oczekujemy na potwierdzenie, że przesyłka dotarła do exchange
-//            channel.WaitForConfirmsOrDie();
-//            Console.WriteLine($"Dodałem dla: {newAccount.EmailAddress} i passwordHash: {newAccount.PasswordHash}");
-//        }
-//    }
-//}
+        public override void Process(IndicatorCalculationElements elements)
+        {
+            try
+            {
+                using (var connection = RabbitMQConnectionHelper.GetConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        while (true)
+                        {
+                        //umożliwia potwierdzanie, że przesyłkę dostarczono do Exchange
+                        channel.ConfirmSelect();
+
+                        //generujemy nowe konto
+                        GenerateAndPublishMessage(channel, elements);
+
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public override void GenerateAndPublishMessage(IModel channel, IndicatorCalculationElements indicatorElements)
+        {
+            //serializujemy oraz przekształcamy w tablicę bajtów
+            var body = EncryptionHelper.StringToUtf8(JsonSerializer.CollectionOfQuotesWithParametersToJsonString(indicatorElements));
+            channel.BasicPublish(exchange: _exchange,
+                routingKey: _queueSendTo,
+                basicProperties: null,
+                body: body);
+            //oczekujemy na potwierdzenie, że przesyłka dotarła do exchange
+            channel.WaitForConfirmsOrDie();
+            Console.WriteLine($"Przesłałem wskaźnik do obliczenia");
+        }
+    }
+}
