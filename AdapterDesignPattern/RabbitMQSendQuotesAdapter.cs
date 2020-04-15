1d5222d58990292e747f9425895a18a8ec395b26
Łukasz Czepielik <koragrex@gmail.com>
2020-04-15 20:12:42 +0200

    Added RabbitMQSendQuotesAdapter with proper interface.

--- AdapterDesignPattern/RabbitMQSendQuotesAdapter.cs
+++ AdapterDesignPattern/RabbitMQSendQuotesAdapter.cs
@@ -1,8 +1,30 @@
-﻿using System;
+﻿using Models;
+using RabbitMQ;
+using System;
+using System.Collections.Generic;
 
 namespace AdapterDesignPattern
 {
-    public class RabbitMQSendQuotesAdapter
+    public class RabbitMQSendQuotesAdapter : IRabbitMQSendQuotesAdapter
     {
+        private IRabbitMQSendQuotesToCalculation _producer { get; set; }
+
+        public void SendQuotesToCalculationInConsumer(List<Quote> quotes, Parameters parameters,
+                                                      string exchange, string queueSendTo)
+        {
+            IndicatorCalculationElements elements = new IndicatorCalculationElements
+            {
+                Quotes = quotes,
+                Parameters = parameters
+            };
+
+            InitializeProducer(exchange, queueSendTo);
+            _producer.Process(elements);
+        }
+
+        private void InitializeProducer(string exchange, string queueSendTo)
+        {
+            _producer = new RabbitMQSendQuotesToCalculation(exchange, queueSendTo);
+        }
     }
 }
