5ed99799e41804e6981d82f2ed439100dfba65aa
Łukasz Czepielik <koragrex@gmail.com>
2020-05-03 01:12:47 +0200

    Added FileHelper and new empty WebApi Core 3.1 project.

---
+++ Utility/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Utility
+{
+    public static class FileHelper
+    {
+        public static string ReadFile(string url)
+        {
+            string fileText = "";
+
+            if (!File.Exists(url))
+            {
+                fileText = File.ReadAllText(url);
+            }
+
+            return fileText;
+        }
+    }
+}
