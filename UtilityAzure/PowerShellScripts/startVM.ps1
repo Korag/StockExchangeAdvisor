dfa84e386869c455c54ea59c4442f66607340a9c
Łukasz Czepielik <koragrex@gmail.com>
2020-05-03 01:13:32 +0200

    Added AzureWebServiceHelper with script needed in order to start and stop virtual machine located on Azure.

---
+++ UtilityAzure/PowerShellScripts/startVM.ps1
@@ -0,0 +1,16 @@
+Import-AzureRmContext -Path "E:\Projects\Visual Studio 2019\StockExchangeAdvisor\Utility\PowerShellScripts\azureprofile.json" 
+ 
+$PowerState = ((Get-AzureRmVM -Name Centos -ResourceGroupName WebServices -
+Status).Statuses[1]).code 
+ 
+If ( $PowerState -contains "PowerState/running") 
+{ 
+   Write-Host "PowerState1: running" 
+} 
+ElseIf ( $PowerState -contains "PowerState/deallocated") 
+{ 
+   Start-AzureRmVM -Name Centos -ResourceGroupName WebServices 
+   $PowerState = ((Get-AzureRmVM -Name Centos -ResourceGroupName WebServices -
+Status).Statuses[1]).code 
+} 
+Write-Host "PowerState2: $PowerState"
