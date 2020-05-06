c117ead24524671607f090c9296152afdb179fcb
Łukasz Czepielik <koragrex@gmail.com>
2020-05-06 18:21:07 +0200

    Added concrete decorators classes.

--- StateAndDecoratorDesignPatterns/Decorator/ConversionFromPLNtoEURDecorator.cs
+++ StateAndDecoratorDesignPatterns/Decorator/ConversionFromPLNtoEURDecorator.cs
@@ -7,5 +7,25 @@
         {
             
         }
+
+        public override double CalculateConst()
+        {
+            double conversionFee = _baseComponent.CalculateConst() * 0.03;
+
+            switch (_baseComponent.GetState().SignalValue)
+            {
+                case 1:
+                    return _baseComponent.CalculateConst() - conversionFee;
+
+                case -1:
+                    return _baseComponent.CalculateConst() + conversionFee;
+
+                default:
+                case 0:
+                    return _baseComponent.CalculateConst();
+            }
+
+            //return _baseComponent.CalculateConst() + 1000;
+        }
     }
 }
--- StateAndDecoratorDesignPatterns/Decorator/TaxDecorator.cs
+++ StateAndDecoratorDesignPatterns/Decorator/TaxDecorator.cs
@@ -7,5 +7,25 @@
         {
            
         }
+
+        public override double CalculateConst()
+        {
+            double taxFee = _baseComponent.CalculateConst() * 0.18;
+
+            switch (_baseComponent.GetState().SignalValue)
+            {
+                case 1:
+                    return _baseComponent.CalculateConst() - taxFee;
+
+                case -1:
+                    return _baseComponent.CalculateConst() + taxFee;
+
+                default:
+                case 0:
+                    return _baseComponent.CalculateConst();
+            }
+
+            //return _baseComponent.CalculateConst() + 1000;
+        }
     }
 }
