57555bbbb1b760711a60ad773d6cf7a1c11e8d4a
Łukasz Czepielik <koragrex@gmail.com>
2020-05-19 00:34:55 +0200

    Added new TechnicalIndicators classes.

---
+++ TechnicalIndicators/TechnicalIndicatorCandleFormation.cs
@@ -0,0 +1,52 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using TechnicalIndicators;
+
+namespace Signals
+{
+    public class CandleFormation : TechnicalIndicator
+    {       
+
+        private const int SELL = -1;
+        private const int BUY = 1;
+
+        public override List<Signal> GetSignals(List<Quote> quote, Parameters parameters)
+        {
+            List<Signal> SignalList = new List<Signal>();
+
+            /*Quote first = new Quote();
+            first.Open = 13.70;
+            first.Close = 14.00;
+            first.Low = 14.00;
+            first.High = 16.00;*/
+
+            for (int i = parameters.NQuotesBackwards; i < quote.Count; i++)
+            {
+                Signal test = new Signal
+                {
+                    Date = quote[i].Date
+                };
+
+                double shadow_low = quote[i].Close - quote[i].Low;
+                double shadow_high = quote[i].High - quote[i].Open;
+
+                double bodySize = Math.Abs(quote[i].Open - quote[i].Close);
+
+                if (shadow_low > bodySize * 2 && shadow_high < bodySize * 0.1)
+                {
+                    test.Value = SELL;
+                }
+
+                else if (shadow_low < bodySize * 0.1 && shadow_high > bodySize * 2)
+                {
+                    test.Value = BUY;
+                }
+
+                SignalList.Add(test);
+            }
+
+            return SignalList;
+        }
+    }
+}
---
+++ TechnicalIndicators/Wskaznik_RSI.cs
@@ -0,0 +1,70 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using TechnicalIndicators;
+
+namespace Signals
+{
+    public class RSI : TechnicalIndicator
+    {
+
+        private const int SELL = -1;
+        private const int STAY = 0;
+        private const int BUY = 1;
+
+        public override List<Signal> GetSignals(List<Quote> quote, Parameters parameters)
+        {
+            List<Signal> Signal_test = new List<Signal>();
+
+            for (int i = parameters.NQuotesBackwards; i < quote.Count; i++)
+            {
+                Signal signal_1 = new Signal
+                {
+                    Date = quote[i].Date
+                };
+
+                double value, sum, value_1, sum_1;
+                //double[] rise_values =  quote[i].Close_Rise;                                //Wartości wzrostu cen
+                //double rise_count = rise_values.Length;                                     //Ilość wartości wzrostu cen
+                //double[] decrease_values = quote[i].Close_Decrease;                         //Wartości spadku cen
+                //double decrease_count = decrease_values.Length;                             //Ilość wartości spadku cen
+                double a = 0;
+                double b = 0;
+
+                //for (value = 0; value <= rise_count - 1; value++)                           //Pętla z której otrzymujemy wartość a = średnia wartość wzrostu cen zamknięcia
+                //{
+                //    sum += rise_values[1];
+                //    a = sum / rise_count;
+                //}
+
+                //for (value_1 = 0; value_1 <= decrease_count - 1; value_1++)                 //Pętla z której otrzymujemy wartość b = średnia wartość spadku cen zamknięcia
+                //{
+                //    sum_1 += decrease_values[1];
+                //    b = sum / decrease_count;
+                //}
+
+                double rs = a / b;                                                          //Wyliczanie rs, potrzebne do wyliczenia wartość RSI
+                double rsi = 100 - 100 / (1 + rs);                                          //Wyliczanie wartości RSI
+
+                if (rsi <= 30)                                                              //Jeżeli wartość RSI jest mniejsza niż 30, jest to sygnał kupna
+                {
+                    signal_1.Value = BUY;
+                }
+
+                else if (rsi >= 70)                                                         //Jeżeli wartość RSI jest większa niż 70, jest to sygnał sprzedaży
+                {
+                    signal_1.Value = SELL;
+                }
+                
+                else if (rsi < 70 || rsi > 30)                                              //Jeżeli wartość RSI zawiera się pomiędzy 30 a 70, nie należy podejmować żadnych decyzji
+                {
+                    signal_1.Value = STAY;
+                }
+
+                Signal_test.Add(signal_1);
+            }
+
+            return Signal_test;
+        }
+    }
+}
